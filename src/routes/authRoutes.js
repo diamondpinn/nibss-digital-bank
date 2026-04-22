8ec9e5125927a6bc92fb00e1485185346899c5c0
diamondpinn <diamondpinxy@gmail.com>
2026-04-22 19:14:42 +0100

    Cleanup: Reverted to stable banking UI

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,8 @@
 ﻿const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
-const { onboardNibss } = require('../controllers/onboardController');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/onboard-nibss', onboardNibss);
 
 module.exports = router;
